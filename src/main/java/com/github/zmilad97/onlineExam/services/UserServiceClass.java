0587108a0d399625cc387a957203597144343dae
milad <zmilad97@gmail.com>
2021-02-02 19:01:30 +0330

    added updateUser method in UserServiceClass

--- src/main/java/com/github/zmilad97/onlineExam/services/UserServiceClass.java
+++ src/main/java/com/github/zmilad97/onlineExam/services/UserServiceClass.java
@@ -1,6 +1,7 @@
 package com.github.zmilad97.onlineExam.services;
 
 import com.github.zmilad97.onlineExam.module.User;
+import com.github.zmilad97.onlineExam.security.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,12 +23,14 @@ public class UserServiceClass {
     }
 
     public void addUser(User user){
+        if(!(userService.existsById(user.getId()))){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setActive(true);
         user.setPermissions(new ArrayList<>());
         user.addPermission("user");
         user.setRoles("USER");
         userService.save(user);
+        }
     }
 
     public void setRole(long userId, String role) {
@@ -45,6 +48,11 @@ public class UserServiceClass {
     public void givePermission(List<Long> studentsId, String examId) {
         studentsId.forEach(id -> userService.findUserById(id).addPermission(examId));
         studentsId.forEach(id -> userService.save(userService.findUserById(id)));
+    }
+
+    public void updateUser(User user){
+        if(SecurityUtil.getCurrentUser().getId() == user.getId() || SecurityUtil.getCurrentUser().getRoles().equals("ADMIN"))
+            userService.save(user);
     }
 
 
